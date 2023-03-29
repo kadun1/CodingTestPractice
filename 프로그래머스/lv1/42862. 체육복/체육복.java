8aec4a0f4e141af29568ed62290557db09b2c7fc
SeongJun Kim <73927286+kadun1@users.noreply.github.com>
2023-03-29 22:16:33 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+                int[] members = new int[n];
+
+        for (int i = 0; i < members.length; i++) {
+            members[i] = 1;
+        }
+
+        for (int i = 0; i < reserve.length; i++) {
+            members[reserve[i]-1] += 1;
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            members[lost[i]-1] -= 1;
+        }
+
+        for (int i = 0; i < members.length; i++) {
+            if(i==0){
+                if(members[i]==0&&members[i+1]==2){
+                    members[i]+=1;
+                    members[i+1]-=1;
+                }
+            } else if(i!=members.length-1){
+                if(members[i]==0&&members[i-1]==2) {
+                    members[i] += 1;
+                    members[i - 1] -= 1;
+                } else if(members[i]==0&&members[i+1]==2){
+                    members[i]+=1;
+                    members[i+1]-=1;
+                } else if(members[i]==0&&members[i-1]==2){
+                    members[i]+=1;
+                    members[i-1]-=1;
+                }
+            } else {
+                if(members[i]==0&&members[i-1]==2){
+                    members[i]+=1;
+                    members[i-1]-=1;
+                }
+            }
+        }
+
+        for (int i = 0; i < members.length; i++) {
+            if(members[i]>0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
