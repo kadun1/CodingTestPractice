bae7128f794945864c5f018e5e49efeb9c7f2d09
SeongJun Kim <73927286+kadun1@users.noreply.github.com>
2023-07-18 21:22:49 +0900

    [unrated] Title: 정수 찾기, Time: 0.86 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        return Arrays.stream(num_list).filter(i -> i==n).count() > 0 ? 1 : 0;
+    }
+}
